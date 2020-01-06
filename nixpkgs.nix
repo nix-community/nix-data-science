09e74e65510570c4e98d0c11e1eda9d2870d5a8b
Tyler Benster <tbenst@gmail.com>
2020-01-05 19:07:13 -0800

    nixpkgs: nixpkgs-unstable channel with cherry-pick patches

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,6 @@
 let
-  nixpkgsSHA = "7fa339238f00784aba7d15c7a369792aecdf305a";
+  # now tracking https://github.com/tbenst/nixpkgs/tree/nix-data
+  nixpkgsSHA = "94a36f880665a0922fcb940531eda2e55715291a";
   pkgs = import (fetchTarball
     "https://github.com/tbenst/nixpkgs/archive/${nixpkgsSHA}.tar.gz") {
       system = builtins.currentSystem;
